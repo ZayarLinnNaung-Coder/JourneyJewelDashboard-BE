4c952c8c4ce0f6becea7b7f988a1b83624e70b26
Zayar Linn Naung <linnnaung233z@prestigein.com>
2025-08-25 19:32:23 +0630

    update

--- src/main/java/root/development/tms/features/profile/service/ProfileService.java
+++
@@ -1,7 +0,0 @@
-package root.development.tms.features.profile.service;
-
-import root.development.tms.features.profile.domain.response.UserProfileResponse;
-
-public interface ProfileService {
-    UserProfileResponse getLoginProfile();
-}
